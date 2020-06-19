6c648e6f0cfc50b91277ec798758efb63230704b
dDenysS <denv928@gmail.com>
2020-06-19 22:45:21 +0300

    update gulp tasks

--- gulp/tasks/webp.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const gulpWebp = require('gulp-webp');
-
-module.exports = function webp() {
-  return gulp.src('src/img/*.{png,jpg}')
-    .pipe(gulpWebp())
-    .pipe(gulp.dest('build/img'))
-}
--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,20 @@ const imageMinify = require('./gulp/tasks/imageMinify')
 const clean = require('./gulp/tasks/clean')
 const copyDependencies = require('./gulp/tasks/copyDependencies')
 const lighthouse = require('./gulp/tasks/lighthouse')
+const svgSprite = require('./gulp/tasks/svgSprite')
 
-const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify)
+function setMode(isProduction = false) {
+  return cb => {
+    process.env.NODE_ENV = isProduction ? 'production' : 'development'
+    cb()
+  }
+}
+
+const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify, svgSprite)
 
 const build = gulp.series(clean, copyDependencies, dev)
 
-module.exports.start = gulp.series(build, serve)
-module.exports.build = build
+module.exports.start = gulp.series(setMode(), build, serve)
+module.exports.build = gulp.series(setMode(true), build)
 
 module.exports.lighthouse = gulp.series(lighthouse)
