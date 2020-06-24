c59b29dbeabcf87ad8655c5f52690d9973ec5224
katylorny <katy_lorny@mail.ru>
2020-06-24 04:38:21 +0300

    новые картинки и заголовок

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -2,8 +2,8 @@ const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const pugLinter = require('gulp-pug-linter')
-const htmlValidator = require('gulp-w3c-html-validator')
-const bemValidator = require('gulp-html-bem-validator')
+// const htmlValidator = require('gulp-w3c-html-validator')
+// const bemValidator = require('gulp-html-bem-validator')
 const config = require('../config')
 
 module.exports = function pug2html() {
@@ -11,8 +11,8 @@ module.exports = function pug2html() {
     .pipe(plumber())
     .pipe(pugLinter({ reporter: 'default' }))
     .pipe(pug({ pretty: config.pug2html.beautifyHtml }))
-    .pipe(htmlValidator())
-    .pipe(bemValidator())
+    // .pipe(htmlValidator())
+    // .pipe(bemValidator())
     .pipe(gulp.dest('build'))
 }
 
